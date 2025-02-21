ad1af3fae64249bd41b893283b87d6780fecaa3e
MrjazTm <63402636+mrjaztm@users.noreply.github.com>
2025-02-21 17:08:38 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
 
-    async redirects() {
-      return [
-        {source: '/next/unos',destination: 'https://mrjaz.com',permanent: true,},
-      ]
-    },
 
   
 };
